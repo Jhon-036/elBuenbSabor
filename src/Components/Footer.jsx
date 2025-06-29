3a9ab7081fc5d63e1ef730820d320aa74e0aab72
Jhon-036 <jhondavid19p@gmail.com>
2025-06-29 01:16:37 -0500

    avance perfil admin

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import '../Styles/HeroSection.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2025 Nombre del Restaurante. Todos los derechos reservados.</p>
+    </footer>
+  );
+};
+
+export default Footer;
